af9cf82846e31a71a0bb5520a3c263d432920ee3
kavyac32 <25bcs032@kgcas.com>
2025-09-11 21:14:18 +0530

    unit 2

---
+++ data types .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	float b=3.14;
+	char c='A';
+	double d=20.123456;
+	printf("values of variables:\n");
+	printf("integer:%d\n",a);
+	printf("float:%.2f\n",b);
+	printf("character:%c\n",c);
+	printf("double:%6f\n",d);
+	
+	return 0;
+	
+}
